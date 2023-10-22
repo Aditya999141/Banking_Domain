db53c760457c87ff246da0f3f5f250c5869d77d6
Aditya999141 <147358321+Aditya999141@users.noreply.github.com>
2023-10-22 16:42:18 +0530

    HomePageAndLoginPageAdded

--- src/main/java/com/base/baseclass.java
+++ src/main/java/com/base/baseclass.java
@@ -1,16 +1,51 @@
 package com.base;
 
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import com.utility.Utility;
 
-public class baseclass
-{
+public class baseclass {
+	public static WebDriver driver;
+	public static Properties prop;
 	
-	public static void main(String[] args)
-	{
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-		driver.manage().window().maximize();
-	}
+	
+	
+	
+	public static void launchTheWeb()
+	
+	 
+    {
+		 FileInputStream file=null;;
+			try {
+				File src = new File("C:\\Users\\Lenovo\\eclipse-workspace\\BankingDemo\\src\\main\\resources\\Property\\configure.properties");
+				file = new FileInputStream( src);
+			} catch (FileNotFoundException e) {
+				
+				e.printStackTrace();
+			}
+			   prop= new Properties();
+			   try {
+				prop.load(file);
+			} catch (IOException e) {
+				
+				e.printStackTrace();
+			}   // load properties file
+	
+    	
+    	driver= new ChromeDriver();
+    	
+    	driver.manage().window().maximize();
+    	Utility.implicit_Wait();
+    	driver.get(prop.getProperty("url"));
+    	
+    }  	
 
 }
